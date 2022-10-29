fd59e8b306211bfdb41538c0b84c51b08be28ed6
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2022-10-29 14:33:11 -0500

    Added Description for Movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ more_movies = [
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'},
   {:title => 'Grave of the Fireflies', :rating => 'Not Rated',
-    :release_date => '16-Apr-1988'},
+    :release_date => '16-Apr-1988', :description => 'A devastating meditation on the human cost of war, this animated tale follows Seita (Tsutomu Tatsumi), a teenager charged with the care of his younger sister, Setsuko (Ayano Shiraishi), after an American firebombing during World War II separates the two children from their parents. Their tale of survival is as heartbreaking as it is true to life. The siblings rely completely on each other and struggle against all odds to stay together and stay alive.'},
   {:title => 'Avengers: Endgame', :rating => 'PG-13',
-    :release_date => '26-Apr-2019'}
+    :release_date => '26-Apr-2019', :description => 'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.'}
 ]
 
 more_movies.each do |movie|
